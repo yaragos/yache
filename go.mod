c409caba91145541d181d590777b747bca5667d8
yaragos <yaid1410@foxmail.com>
2024-10-08 21:22:43 +0800

    Init.

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/yaragos/yache
+
+go 1.21
+
+toolchain go1.23.2
+
+require google.golang.org/protobuf v1.35.1
---
+++ peers.go
@@ -0,0 +1,14 @@
+package yache
+
+import pb "github.com/yaragos/yache/yachepb"
+
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool)
+}
+
+// PeerGetter is the interface that must be implemented by a peer.
+type PeerGetter interface {
+	Get(in *pb.Request, out *pb.Response) error
+}
